4ef5dfded2a3e1311c68f3578b930f83823717d1
alex <khorunzhiyal@gmail.com>
2025-06-04 09:51:37 +0300

    rocket engine; acceptable

---
+++ assets/shaders/rocket_fire.wgsl
@@ -0,0 +1,194 @@
+#import bevy_pbr::{
+    pbr_fragment::pbr_input_from_standard_material,
+    pbr_functions::alpha_discard,
+    mesh_view_bindings,
+}
+
+#ifdef PREPASS_PIPELINE
+#import bevy_pbr::{
+    prepass_io::{VertexOutput, FragmentOutput},
+    pbr_deferred_functions::deferred_output,
+}
+#else
+#import bevy_pbr::{
+    forward_io::{VertexOutput, FragmentOutput},
+    pbr_functions::{apply_pbr_lighting, main_pass_post_lighting_processing},
+}
+#endif
+
+@group(2) @binding(100)
+var<uniform> color: vec4f;
+@group(2) @binding(101)
+var<uniform> center: vec4f;
+@group(2) @binding(102)
+var<uniform> nof_particles: vec4u;
+@group(2) @binding(103)
+var<uniform> particles: array<vec4f, 32>;
+@group(2) @binding(104)
+var<uniform> dir: vec4f;
+
+const PARTICLE_RADIUS: f32 = 2.0;
+
+
+
+fn snoise3(p: vec3<f32>) -> vec3<f32> {
+    return vec3f(snoise(p), snoise(p + vec3f(100.0)), 0.0);
+}
+
+fn density_at_point(point: vec3f) -> f32 {
+    let t = mesh_view_bindings::globals.time;
+
+    let color = color.xyz;
+    let center = center.xyz;
+    let nof_particles = nof_particles.x;
+
+    let local_point = point - center;
+    let dist_to_engine = distance(point, center);
+
+    var density = 0.0;
+    for (var i = 0u; i < nof_particles; i++) {
+        var particle_pos = particles[i].xyz;
+        particle_pos += snoise3(particle_pos) * dist_to_engine * 0.1;
+
+        var dist = distance(point, particle_pos);
+
+        if (dist > PARTICLE_RADIUS) {
+          continue;
+        }
+
+        dist /= PARTICLE_RADIUS;
+
+        let curr_density = 1.0 - dist;
+
+        let particle_dist = distance(particle_pos, center);
+        let particle_intensity = 2.0 / particle_dist;
+
+        density += curr_density * particle_intensity;
+    }
+
+    
+    density = min (1.0, density);
+
+    var mask = dot(-dir.xy, normalize(local_point.xy));
+    mask = smoothstep(0.4, 0.9, mask);
+    density =  density * mask;
+
+    return density;
+}
+
+@fragment
+fn fragment(
+    in: VertexOutput,
+    @builtin(front_facing) is_front: bool,
+) -> FragmentOutput {
+    var color = color.xyz;
+    let center = center.xyz;
+  
+    let pbr_input = pbr_input_from_standard_material(in, is_front);
+    // let light_direction = mesh_view_bindings::lights.directional_lights[0].direction_to_light;
+    
+    var position = pbr_input.world_position.xyz;
+
+    var out: FragmentOutput;
+
+    var density = 0.0;
+
+    density += density_at_point(position);
+
+    if (density < 0.01) {
+        return out;
+    }
+
+
+
+    out.color = mix(vec4f(color, 0.0), vec4f(1.0), density);
+    if (density > 0.8) {
+      out.color = mix(vec4f(color, 0.3), vec4f(1.0), 0.4);
+    } else if (density > 0.65) {
+      out.color = mix(vec4f(color, 0.7), vec4f(1.0), 0.99);
+    } else if (density > 0.5) {
+      out.color = mix(vec4f(color, 0.6), vec4f(1.0), 0.7);
+    } else if (density > 0.4) {
+      out.color = mix(vec4f(color, 0.5), vec4f(1.0), 0.6);
+    } else if (density > 0.3) {
+      out.color = mix(vec4f(color, 0.4), vec4f(1.0), 0.4);
+    } else {
+      out.color = vec4f(0.0);
+    }
+
+    return out;
+    
+
+    
+
+    // pbr_input.material.base_color.b = pbr_input.material.base_color.r;
+    // pbr_input.material.base_color = alpha_discard(pbr_input.material, pbr_input.material.base_color);
+    // var out: FragmentOutput;
+    // out.color = apply_pbr_lighting(pbr_input);
+    // out.color = main_pass_post_lighting_processing(pbr_input, out.color);
+    // out.color = out.color * 2.0;
+    // return out;
+}
+
+
+
+
+// Translated from Shadertoy: <www.shadertoy.com/view/XsX3zB>
+// by Nikita Miropolskiy
+
+/* discontinuous pseudorandom uniformly distributed in [-0.5, +0.5]^3 */
+fn random3(c: vec3<f32>) -> vec3<f32> {
+    let j = 4096.0 * sin(dot(c, vec3<f32>(17.0, 59.4, 15.0)));
+    var r: vec3<f32>;
+    r.z = fract(512.0 * j);
+    let j1 = j * 0.125;
+    r.x = fract(512.0 * j1);
+    let j2 = j1 * 0.125;
+    r.y = fract(512.0 * j2);
+    return r - 0.5;
+}
+
+const F3: f32 = 0.3333333;
+const G3: f32 = 0.1666667;
+
+fn snoise(p: vec3<f32>) -> f32 {
+    let s = floor(p + dot(p, vec3<f32>(F3)));
+    let x = p - s + dot(s, vec3<f32>(G3));
+    
+    let e = step(vec3<f32>(0.0), x - x.yzx);
+    let i1 = e * (1.0 - e.zxy);
+    let i2 = 1.0 - e.zxy * (1.0 - e);
+    
+    let x1 = x - i1 + G3;
+    let x2 = x - i2 + 2.0 * G3;
+    let x3 = x - 1.0 + 3.0 * G3;
+    
+    var w: vec4<f32>;
+    var d: vec4<f32>;
+    
+    w.x = dot(x, x);
+    w.y = dot(x1, x1);
+    w.z = dot(x2, x2);
+    w.w = dot(x3, x3);
+    
+    w = max(0.6 - w, vec4<f32>(0.0));
+    
+    d.x = dot(random3(s), x);
+    d.y = dot(random3(s + i1), x1);
+    d.z = dot(random3(s + i2), x2);
+    d.w = dot(random3(s + vec3<f32>(1.0)), x3);
+    
+    w = w * w;
+    w = w * w;
+    d = d * w;
+    
+    return dot(d, vec4<f32>(52.0));
+}
+
+
+fn snoiseFractal(m: vec3<f32>) -> f32 {
+    return 0.5333333 * snoise(m)
+         + 0.2666667 * snoise(2.0 * m)
+         + 0.1333333 * snoise(4.0 * m)
+         + 0.0666667 * snoise(8.0 * m);
+}
--- src/rocket_fire.rs
+++ src/rocket_fire.rs
@@ -1,45 +1,74 @@
+use std::{cmp::Ordering, f32::consts::PI, time::Duration};
+
+use avian2d::{PhysicsPlugins, math::Vector, prelude::*};
 use bevy::{
-    color::palettes::css::{GREEN, RED},
+    color::palettes::css::{GREEN, PURPLE, RED},
+    math::VectorSpace,
     pbr::{ExtendedMaterial, MaterialExtension},
     prelude::*,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    reflect::hash_error,
+    render::render_resource::{AsBindGroup, Face, ShaderRef},
+    scene::SceneInstance,
+    time::common_conditions::on_timer,
 };
 use bevy_egui::{EguiContextPass, EguiContexts, egui};
 use bevy_fly_camera::FlyCamera;
+use bevy_tweening::{Animator, Delay, Tween, TweenCompleted};
 
 const FIRE_SHADER_PATH: &str = "shaders/rocket_fire.wgsl";
+const NOF_PARTICLES: usize = 20;
+const PARTICLE_SPEED: f32 = 50.;
 
 pub struct RocketFirePlugin;
 
 impl Plugin for RocketFirePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((MaterialPlugin::<
-            ExtendedMaterial<StandardMaterial, FireMaterialExtension>,
-        >::default(),))
-            .add_systems(EguiContextPass, build_sliders)
-            .add_systems(Startup, setup_scene)
-            .add_systems(Update, on_params_changed.run_if(resource_changed::<Params>))
-            .add_observer(on_spawn_fire)
-            .init_resource::<Params>();
+        app.add_plugins((
+            MaterialPlugin::<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>::default(),
+            PhysicsPlugins::default(),
+        ))
+        .add_systems(EguiContextPass, build_sliders)
+        .add_systems(Startup, setup_scene)
+        .add_systems(Update, on_params_changed.run_if(resource_changed::<Params>))
+        .add_systems(Update, customize_scene_materials)
+        .add_systems(Update, set_shader_params)
+        .add_systems(
+            Update,
+            spawn_particle.run_if(on_timer(Duration::from_millis(20))),
+        )
+        .init_resource::<Params>();
     }
+}
+
+#[derive(Component)]
+struct Particle {
+    id: usize,
 }
 
 #[derive(Asset, AsBindGroup, Reflect, Debug, Clone)]
 struct FireMaterialExtension {
     #[uniform(100)]
-    progress: Vec4,
+    color: Vec4,
     #[uniform(101)]
     center: Vec4,
     #[uniform(102)]
-    radius: Vec4,
+    nof_particles: UVec4,
+
+    #[uniform(103)]
+    particles: [Vec4; 32],
+
+    #[uniform(104)]
+    dir: Vec4,
 }
 
 impl Default for FireMaterialExtension {
     fn default() -> Self {
         Self {
-            progress: Vec4::new(0., 0.0, 0.0, 0.0),
-            radius: Vec4::new(20., 0.0, 0.0, 0.0),
-            center: Vec4::ZERO,
+            color: Vec4::new(0., 0.0, 0.0, 0.0),
+            center: Vec4::new(0.0, 0.0, 0.0, 0.0),
+            nof_particles: UVec4::new(NOF_PARTICLES as u32, 0, 0, 0),
+            particles: [Vec4::ZERO; 32],
+            dir: Vec3::Y.extend(0.0),
         }
     }
 }
@@ -54,28 +83,35 @@ impl MaterialExtension for FireMaterialExtension {
     }
 }
 
-#[derive(Event)]
-struct MeteoriteExplosion {
-    position: Vec2,
+#[derive(Component)]
+struct Rocket {}
+
+#[derive(Component)]
+struct Fire {}
+
+#[derive(Component)]
+struct CustomizeMaterial {
+    material: Handle<StandardMaterial>,
 }
-
-#[derive(Component)]
-struct Meteorite {}
-
-#[derive(Component)]
-struct Explosion {}
 
 #[derive(Resource)]
 struct Params {
-    progress: f32,
-    radius: f32,
+    rocket_rotation: f32,
+    color: Vec3,
+    center: Vec3,
+    size: f32,
+
+    particle_speed: f32,
 }
 
 impl Default for Params {
     fn default() -> Self {
         Self {
-            progress: 0.0,
-            radius: 20.,
+            rocket_rotation: 0.0,
+            color: Vec3::new(1., 0., 0.),
+            center: Vec3::new(0., 0., 0.),
+            size: 0.1,
+            particle_speed: 50.0,
         }
     }
 }
@@ -84,19 +120,40 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut explosion_materials: ResMut<
+        Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>,
+    >,
+    asset_server: Res<AssetServer>,
 ) {
+    let rocket_model = asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/Ship1.glb"));
+
     commands.spawn((
-        Meteorite {},
-        Mesh3d(meshes.add(Sphere::new(2.0))),
-        Transform::from_xyz(0., 0., 0.),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: RED.into(),
-            ..Default::default()
-        })),
+        Rocket {},
+        SceneRoot(rocket_model),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)).with_scale(Vec3::ONE * 1.0),
+        // CustomizeMaterial {
+        //     material: materials.add(StandardMaterial {
+        //         base_color: GREEN.into(),
+        //         ..Default::default()
+        //     }),
+        // },
+        children![(
+            Fire {},
+            Transform::from_translation(-Vec3::Y * 0.),
+            // Mesh3d(meshes.add(Sphere::new(10.0))),
+            Mesh3d(meshes.add(Rectangle::from_length(200.))),
+            MeshMaterial3d(explosion_materials.add(ExtendedMaterial {
+                base: StandardMaterial {
+                    alpha_mode: AlphaMode::Blend,
+                    ..Default::default()
+                },
+                extension: FireMaterialExtension { ..default() },
+            })),
+        )],
     ));
 
     commands.spawn((
-        Mesh3d(meshes.add(Sphere::new(6.0))),
+        Mesh3d(meshes.add(Sphere::new(0.5))),
         Transform::from_xyz(5., 30., 10.),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: GREEN.into(),
@@ -118,10 +175,6 @@ fn setup_scene(
         Transform::default().looking_at(Vec3::new(1.0, 0.0, 0.0), Dir3::Y),
     ));
 
-    // commands.trigger(MeteoriteExplosion {
-    //     position: Vec2::ZERO,
-    // });
-
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 0.0, 40.0).looking_at(Vec3::ZERO, Dir3::Y),
@@ -131,65 +184,131 @@ fn setup_scene(
 
 fn on_params_changed(
     params: Res<Params>,
-    mut explosion_materials: ResMut<
-        Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>,
-    >,
-    materials: Query<
+    mut fire_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>>,
+    fire_material: Query<
         &MeshMaterial3d<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>,
-        With<Explosion>,
+        With<Fire>,
     >,
+    mut rocket_transform: Single<&mut Transform, With<Rocket>>,
 ) {
-    let Ok(material) = materials.single() else {
+    let Ok(material) = fire_material.single() else {
         return;
     };
-    let Some(material) = explosion_materials.get_mut(material.id()) else {
+    let Some(material) = fire_materials.get_mut(material.id()) else {
         return;
     };
 
-    material.extension.progress = Vec4::new(params.progress, 0.0, 0.0, 0.0);
-    material.extension.radius = Vec4::new(params.radius, 0.0, 0.0, 0.0);
+    let color = RED.lerp(PURPLE, params.particle_speed / 100.);
+    material.extension.color = color.to_vec4();
+
+    rocket_transform.rotation = Quat::from_rotation_z(PI * params.rocket_rotation);
 }
 
 fn build_sliders(mut contexts: EguiContexts, mut params: ResMut<Params>, mut commands: Commands) {
     egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
-        ui.add(egui::Slider::new(&mut params.progress, 0.0..=1.0).text("progress"));
-        ui.add(egui::Slider::new(&mut params.radius, 1.0..=50.0).text("radius"));
-
-        if ui.button("Spawn explosion").clicked() {
-            println!("spawn explosion clicked");
-            commands.trigger(MeteoriteExplosion {
-                position: Vec2::ZERO,
-            });
-        }
-        // ui.add(egui::Button::new("spawn explosion"));
-
-        // if ui.button("Increment").clicked() {
-        //     ui_state.value += 1.0;
-        // }
+        ui.add(egui::Slider::new(&mut params.rocket_rotation, 0.0..=2.0).text("rotation"));
+        ui.add(egui::Slider::new(&mut params.particle_speed, 10.0..=100.0).text("power"));
+        // ui.add(egui::Slider::new(&mut params.progress, 0.0..=1.0).text("progress"));
+        // ui.add(egui::Slider::new(&mut params.radius, 1.0..=50.0).text("radius"));
     });
 }
 
-fn on_spawn_fire(
-    trigger: Trigger<MeteoriteExplosion>,
+fn customize_scene_materials(
+    unloaded_instances: Query<(Entity, &SceneInstance, &CustomizeMaterial)>,
+    handles: Query<Entity, (With<MeshMaterial3d<StandardMaterial>>, Without<Rocket>)>,
+    // pbr_materials: Res<Assets<StandardMaterial>>,
+    scene_manager: Res<SceneSpawner>,
+    // mut custom_materials: ResMut<Assets<StandardMaterial>>,
+    // mut custom_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>>,
+    mut cmds: Commands,
+) {
+    for (entity, instance, customize_material) in unloaded_instances.iter() {
+        if scene_manager.instance_is_ready(**instance) {
+            cmds.entity(entity).remove::<CustomizeMaterial>();
+        }
+
+        // Iterate over all entities in scene (once it's loaded)
+        let handles = handles.iter_many(scene_manager.iter_instance_entities(**instance));
+        for entity in handles {
+            // let Some(material) = pbr_materials.get(material_handle) else {
+            //     continue;
+            // };
+
+            cmds.entity(entity)
+                .remove::<MeshMaterial3d<StandardMaterial>>()
+                .insert(MeshMaterial3d(customize_material.material.clone()));
+
+            // let custom = custom_materials.add(material.into());
+            // cmds.entity(entity)
+            //     .insert(custom)
+            //     .remove::<Handle<StandardMaterial>>();
+        }
+    }
+}
+
+fn spawn_particle(
     mut commands: Commands,
+    rocket_transform: Single<&Transform, With<Rocket>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut explosion_materials: ResMut<
-        Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut last_id: Local<usize>,
+    params: Res<Params>,
+    particles: Query<(Entity, &Particle)>,
+) {
+    let particles = particles.iter().collect::<Vec<(Entity, &Particle)>>();
+    if particles.len() > NOF_PARTICLES {
+        let mut oldest_partile_entity: Option<Entity> = None;
+        let mut oldest_id = usize::MAX;
+        for (entity, particle) in particles {
+            if particle.id < oldest_id {
+                oldest_partile_entity = Some(entity);
+                oldest_id = particle.id;
+            }
+        }
+
+        if let Some(oldest_partile_entity) = oldest_partile_entity {
+            commands.entity(oldest_partile_entity).despawn();
+        }
+    }
+
+    commands.spawn((
+        RigidBody::Kinematic,
+        LinearVelocity(-PARTICLE_SPEED * rocket_transform.rotation.mul_vec3(Vec3::Y).xy()),
+        Transform::from_translation(
+            rocket_transform.translation
+                - rocket_transform.rotation.mul_vec3(Vec3::Y) * params.particle_speed * 0.02,
+        ),
+        // Mesh3d(meshes.add(Sphere::new(0.2))),
+        // MeshMaterial3d(materials.add(StandardMaterial {
+        //     base_color: RED.into(),
+        //     ..Default::default()
+        // })),
+        Particle { id: *last_id },
+    ));
+
+    *last_id += 1;
+}
+
+fn set_shader_params(
+    particles: Query<(&Transform, &Particle)>,
+    rocket_transform: Single<&Transform, With<Rocket>>,
+    mut fire_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>>,
+    fire_material: Single<
+        &MeshMaterial3d<ExtendedMaterial<StandardMaterial, FireMaterialExtension>>,
+        With<Fire>,
     >,
 ) {
-    commands.spawn((
-        Explosion {},
-        Transform::from_translation(trigger.position.extend(0.)),
-        Mesh3d(meshes.add(Sphere::new(20.0))),
-        MeshMaterial3d(explosion_materials.add(ExtendedMaterial {
-            base: StandardMaterial {
-                alpha_mode: AlphaMode::Blend,
-                ..Default::default()
-            },
-            extension: FireMaterialExtension {
-                center: trigger.position.extend(0.0).extend(0.0),
-                ..default()
-            },
-        })),
-    ));
+    let Some(fire_material) = fire_materials.get_mut(fire_material.id()) else {
+        return;
+    };
+
+    let mut particles = particles.iter().collect::<Vec<(&Transform, &Particle)>>();
+    particles.sort_by(|(_, a), (_, b)| b.id.cmp(&a.id));
+    for (i, (transform, _)) in particles.iter().enumerate() {
+        fire_material.extension.particles[i] = transform.translation.extend(0.0);
+    }
+
+    fire_material.extension.nof_particles = UVec4::new(particles.iter().len() as u32, 0, 0, 0);
+
+    fire_material.extension.dir = rocket_transform.rotation.mul_vec3(Vec3::Y).extend(0.0);
 }
